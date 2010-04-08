278ad4390042d3f72d8d387146a4032ad42bbebf
Matthias Viehweger <kronn@kronn.de>
2010-04-08 09:01:46 +0200

    before-filter and explanation-view added

--- app.rb
+++ app.rb
@@ -1,18 +1,32 @@
+require 'erb'
+
 class HttpInspectApp < Sinatra::Base
   set :environment, ( ENV['RACK_ENV'] || 'development' ).to_sym
   set :root,        File.dirname(__FILE__)
   disable :run
 
-  get '/' do
+  before do
     header_keys = [
       :accept_encoding, :content_charset, :content_length, :cookies,
-      :host, :ip, :media_type, :media_tyoe_params, :params,
+      :host, :ip, :media_type, :media_type_params, :params,
       :query_string, :referer, :request_method, :scheme,
       :url, :user_agent
     ]
     header_array = header_keys.map do |key|
-      "#{key}: #{request[key]}"
+      "#{key}: #{request.send(key).inspect}"
     end
-    header_array.join("\n")
+    @header_list = header_array.join("\n")
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/text' do
+    @header_list
+  end
+
+  get '/html' do
+    "<pre>#{@header_list}</pre>"
   end
 end
